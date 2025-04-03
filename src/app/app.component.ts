aa943fa5b8e084188a8fa2632eb941418b9abeb5
ragtuun1 <vrcguntupalli@gmail.com>
2025-04-03 00:26:17 -0500

    fixing the style and option to add new records

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,9 @@ export class AppComponent {
   searchQuery: string = '';
   totalGuests: number = 0;
 
+  isModalOpen: boolean = false;
+  newRecord: any = {};
+
   onFileChange(event: any): void {
     const file = event.target.files[0];
     if (file) {
@@ -32,18 +35,54 @@ export class AppComponent {
     }
   }
 
-  updateRecord(index: number, field: string, value: any): void {
-    this.data[index][field] =
-      field === 'CheckedIn' ? value.target.checked : value.target.value;
+  updateRecord(record: any, field: string, value: any): void {
+    if (field === 'CheckedIn') {
+      record[field] = value.target.checked;
+    } else {
+      record[field] = Number(value);
+    }
     this.calculateTotal();
   }
 
   calculateTotal(): void {
     this.totalGuests = this.data.reduce(
       (sum, record) =>
-        sum + (parseInt(record.Adults || 0) + parseInt(record.Kids || 0)),
+        sum +
+        (parseInt(record.Adults || '0', 10) + parseInt(record.Kids || '0', 10)),
       0
     );
+  }
+
+  openModal(): void {
+    this.isModalOpen = true;
+    this.newRecord = {
+      'Last Name': '',
+      'First Name': '',
+      'Email Address': '',
+      'Phone no': '',
+      'Membership Level': '',
+      'Payment Mode': '',
+      CheckedIn: false,
+      Adults: 1,
+      Kids: 1,
+    };
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+  }
+
+  addNewRecord(): void {
+    this.data.push(this.newRecord);
+    this.isModalOpen = false;
+    this.calculateTotal();
+  }
+
+  exportToExcel(): void {
+    const worksheet = XLSX.utils.json_to_sheet(this.data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+    XLSX.writeFile(workbook, 'TAM_Membership.xlsx');
   }
 
   get filteredData() {
